ee8eb9b4472afab5d980df5eb883d588ac3bc8db
TakayoshiYamazaki <opal.s2000@gmail.com>
2026-01-05 16:10:47 +0900

    20260105 seeds.rb comment out

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-Status.create!([
-  {name: "完了", disp: 1},
-  {name: "周知", disp: 2},
-  {name: "対応中", disp: 3}
-])
+# Status.create!([
+#   {name: "完了", disp: 1},
+#   {name: "周知", disp: 2},
+#   {name: "対応中", disp: 3}
+# ])
